d447c5c263adba7f628fe65d0a4085b13bddb27d
tural10 <turalja@code.edu.az>
2023-03-18 14:46:02 +0400

    Добавьте файлы проекта.

---
+++ Task_6/Program.cs
@@ -0,0 +1,356 @@
+﻿namespace Task_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+
+            //Console.Write("uzunlugu daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int n);
+            //int[] arr = new int[n];
+            //Console.Write("c-ni daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int c);
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"arr[{i}]");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < c)
+            //    {
+            //        Console.WriteLine(arr[i]);
+            //    }
+            //}
+
+            #endregion
+
+            #region Task 2
+
+            //int sum = 0;
+            //int count = 0;
+
+            //Console.Write("uzunlugu daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int n);
+            //int[] arr = new int[n];
+            //Console.Write("c-ni daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int c);
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"arr[{i}]");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < c)
+            //    {
+            //        sum += arr[i];
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(sum / count);
+
+            #endregion
+
+            #region Task 3
+
+            //int vurma = 1;
+            //int count = 0;
+
+            //Console.Write("uzunlugu daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int n);
+            //int[] arr = new int[n];
+            //Console.Write("c-ni daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int c);
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"arr[{i}]");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > c)
+            //    {
+            //        vurma *= arr[i];
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(vurma / count);
+
+            #endregion
+
+            #region Task 4
+
+
+            //Console.Write("uzunlugu daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int n);
+            //int[] arr = new int[n];
+            //Console.Write("c-ni daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int c);
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"arr[{i}]");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Array.Sort(arr);
+            //Array.Reverse(arr);
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > c)
+            //    {
+            //        Console.WriteLine(arr[i]);
+            //    }
+            //}
+
+
+            #endregion
+
+            #region Task 5
+
+            //Console.Write("uzunlugu daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int n);
+            //int[] arr = new int[n];
+            //Console.Write("c-ni daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int c);
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"arr[{i}]");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //int max = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < arr[max])
+            //    {
+            //        max = i;
+            //    }
+            //}
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < c)
+            //    {
+            //        if (arr[i] > arr[max])
+            //        {
+            //            max = i;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine(arr[max]);
+
+            #endregion
+
+            #region Task 6
+
+            //Console.Write("1ci massivin uzunlugu daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int n1); 
+            //Console.Write("2ci massivin uzunlugu daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int n2);
+
+            //int[] arr1 = new int[n1];
+            //int[] arr2 = new int[n2];
+
+
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    Console.Write($"arr1[{i}]:");
+            //    arr1[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    Console.Write($"arr2[{i}]:");
+            //    arr2[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    for (int j = 0; j < arr2.Length; j++)
+            //    {
+            //        if (arr1[i] == arr2[j] * arr2[j]) 
+            //        {
+            //            Console.WriteLine($"Serti odeyen:{arr1[i]}");
+            //        }
+            //    }
+            //}
+
+            #endregion
+
+            #region Task 7
+
+            //Console.Write("uzunlugu daxil edin:");
+            //double.TryParse(Console.ReadLine(),out double n);
+            //double[] arr = new double[(int)n];
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"arr[{i}]=");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //int max = 0;
+            //int min = 0;
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > arr[max])
+            //        max = i;
+
+            //    if (arr[i] < arr[min])
+            //        min = i;
+            //}
+            //double ed = (arr[max] + arr[min]) / 2;
+            //double hen = (arr[max] * arr[min]) / 2;
+            //Console.WriteLine($"Ededi orta:{ed}");
+            //Console.WriteLine($"Hendesi orta:{hen}");
+
+            #endregion
+
+            #region Task 8
+
+            //int[] arr1 = new int[14];
+            //int[] arr2 = new int[14];
+
+
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    Console.Write($"arr1[{i}]:");
+            //    arr1[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    Console.Write($"arr2[{i}]:");
+            //    arr2[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("==================================================================");    
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    Console.WriteLine($"arr[{i}]:{arr1[i] * arr2[i]}");
+            //}
+
+            #endregion
+
+            #region Task 9
+
+            //int[] arr1 = new int[25];
+
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    Console.Write($"arr1[{i}]:");
+            //    arr1[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("==================================================================");
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    if (i < 8)
+            //    {
+            //        Console.WriteLine($"arr1[{i}]:{arr1[i] * arr1[i]}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"arr2[{i}]:{arr1[i] / 4}");
+            //    }
+            //}
+
+
+            #endregion
+
+            #region Task 10
+
+            //Console.Write("uzunlugu daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int n);
+            //int[] arr = new int[n];
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"arr[{i}]");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //int max = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < arr[max])
+            //    {
+            //        max = i;
+            //    }
+            //}
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < 0)
+            //    {
+            //        if (arr[i] > arr[max])
+            //        {
+            //            max = i;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine(arr[max]);
+
+            #endregion
+
+            #region Task 11
+
+            //Console.Write("uzunlugu daxil edin:");
+            //int.TryParse(Console.ReadLine(), out int n);
+            //int[] arr = new int[n];
+
+            //Random rd = new Random();
+            
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    int random = rd.Next(0, 101);
+            //    arr[i] = random;
+            //}
+            //Array.Sort(arr);
+            //Array.Reverse(arr);
+            //Console.WriteLine("==========================================");
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+
+            //    Console.Write($"arr[{i}]:");
+            //    Console.WriteLine(arr[i]);
+            //}
+
+            #endregion
+
+            #region Task 12 
+
+            //int[] arr1 = new int[20];
+
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    Console.Write($"arr1[{i}]:");
+            //    arr1[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("==================================================================");
+            //int sum = 0;
+            //int vurma = 1;
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    if (i < 10)
+            //    {
+            //        sum += arr1[i];
+            //    }
+            //    else
+            //    {
+            //        vurma *= arr1[i];
+            //    }
+            //}
+            //Console.WriteLine($"Ededi orta:{sum / 10}");
+            //Console.WriteLine($"Hendesi orta:{vurma / 10}");
+            #endregion
+        }
+    }
+}
